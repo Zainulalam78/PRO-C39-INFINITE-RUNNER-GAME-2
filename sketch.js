411d550578e9627562e5792ea3fd77dca2a8fb20
Zainulalam78 <75262604+Zainulalam78@users.noreply.github.com>
2021-04-20 20:10:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,26 +16,26 @@ localStorage["HighestScore"] = 0;
 var gameOver, restart;
 
 function preload(){
-  jumpSound = loadSound("assets/sounds/jump.wav")
-  collidedSound = loadSound("assets/sounds/collided.wav")
+  jumpSound = loadSound("sounds/jump.wav")
+  collidedSound = loadSound("sounds/collided.wav")
   
-  backgroundImg = loadImage("assets/backgroundImg.png")
-  sunAnimation = loadImage("assets/sun.png");
+  backgroundImg = loadImage("backgroundImg.png")
+  sunAnimation = loadImage("sun.png");
 
-  trex_running = loadAnimation("assets/trex_2.png","assets/trex_1.png","assets/trex_3.png");
-  trex_collided = loadAnimation("assets/trex_collided.png");
+  trex_running = loadAnimation("trex_2.png","trex_1.png","trex_3.png");
+  trex_collided = loadAnimation("trex_collided.png");
 
-  groundImage = loadImage("assets/ground.png");
+  groundImage = loadImage("ground.png");
   
-  cloudImage = loadImage("assets/cloud.png");
+  cloudImage = loadImage("cloud.png");
   
-  obstacle1 = loadImage("assets/obstacle1.png");
-  obstacle2 = loadImage("assets/obstacle2.png");
-  obstacle3 = loadImage("assets/obstacle3.png");
-  obstacle4 = loadImage("assets/obstacle4.png");
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
   
-  gameOverImg = loadImage("assets/gameOver.png");
-  restartImg = loadImage("assets/restart.png");
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
 }
 
 function setup() {
@@ -90,7 +90,7 @@ function draw() {
 
   
   if (gameState===PLAY){
-    score = score + Math.round(getFrameRate()/60);
+    score = score + .round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
 
     if(keyDown("space")) {
@@ -144,7 +144,7 @@ function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
     var cloud = createSprite(width+20,height-300,40,10);
-    cloud.y = Math.round(random(100,220));
+    cloud.y = .round(random(100,220));
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
     cloud.velocityX = -3;
@@ -170,7 +170,7 @@ function spawnObstacles() {
     obstacle.velocityX = -(6 + 3*score/100);
 
     //generate random obstacles
-    var rand = Math.round(random(1,2));
+    var rand = .round(random(1,2));
     switch(rand) {
       case 1: obstacle.addImage(obstacle1);
               break;
